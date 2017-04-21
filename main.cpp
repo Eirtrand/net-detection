b576841cd1c9936b2d5d0569f1135de820cd41a1
Eirtrand <eirstr92@gmail.com>
2017-04-21 17:50:25 +0200

    made algorithm for calculation distance to net

--- main.cpp
+++ main.cpp
@@ -3,24 +3,37 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
+#include <algorithm>    // std::sort
+#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
+
 
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
+bool response_comparator(const KeyPoint& p1, const KeyPoint& p2) {
+    return p1.pt.x > p2.pt.x;
+}
 
 
 
-static void doIteration(Mat & img2, Ptr<FeatureDetector>& detector)
+static void doIteration(Mat & img2, Ptr<FeatureDetector>& detector, cv::VideoWriter & output_cap)
 {
 
     vector<KeyPoint> keypoints;
+    vector<KeyPoint> keypoints3;
+    vector<KeyPoint> ropeKeypoints;
+
+
     detector->detect( img2, keypoints );
 
     Mat original = img2.clone();
     Mat morph = img2.clone();
     Mat rope = img2.clone();
+
     //Grayscale matrix
     cv::Mat grayscaleMat (img2.size(), CV_8U);
     cv::cvtColor( img2, grayscaleMat, CV_BGR2GRAY);
@@ -28,9 +41,6 @@ static void doIteration(Mat & img2, Ptr<FeatureDetector>& detector)
     cv::adaptiveThreshold(grayscaleMat, img2 ,100, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY,21,-5);
 
 
-    vector<KeyPoint> keypoints3;
-    vector<KeyPoint> ropeKeypoints;
-    uchar black = 0;
     cv::cvtColor(img2,img2,CV_GRAY2BGR);
 
 
@@ -57,46 +67,69 @@ static void doIteration(Mat & img2, Ptr<FeatureDetector>& detector)
     Mat erosionElement = getStructuringElement(erosion_elem,Size( 2*erosion_size + 1, 2*erosion_size+1 ), Point( erosion_size, erosion_size ) );
     Mat erosionElement2 = getStructuringElement(2,Size( 2*erosion_size2 + 1, 2*erosion_size2+1 ), Point( erosion_size2, erosion_size2 ) );
 
-    //imshow("threshhold2", rope);
+
     erode( rope, rope, erosionElement );
     dilate( rope, rope, delutionElement );
 
-    //imshow("threshhold2", rope);
+
     cv::Mat grayscaleMatRope2 (rope.size(), CV_8U);
     cv::cvtColor( rope, grayscaleMatRope2, CV_BGR2GRAY);
     cv::Mat binaryMatRope2(grayscaleMatRope2.size(), CV_8U);
-    //imshow("threshhold3", grayscaleMatRope2);
     cv::adaptiveThreshold(grayscaleMatRope2, rope ,207, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY,29,-3);
-
 
     erode( rope, rope, erosionElement2 );
     dilate( rope, rope, delutionElement );
     
     cv::cvtColor(rope,rope,CV_GRAY2BGR);
+
+
     // match keypoint to threshhold image 
+    uchar black = 0;
+    keypoints.erase(keypoints.end() - keypoints.size()/5, keypoints.end() - 1);
+
     for (size_t i = 0; i < keypoints.size(); ++i){
          Point2f pixel = keypoints[i].pt;
          Vec3b colour =  img2.at<Vec3b>(pixel.y,pixel.x);
          Vec3b ropeColor = rope.at<Vec3b>(pixel.y,pixel.x);
-         cout << ropeColor << endl;
-         if(colour.val[0] != black){
-                keypoints3.push_back(keypoints[i]);
-                //circle(keypointsImg2, pixel, 1, CV_RGB(255,156,0),2);
-            }   
 
         if(ropeColor.val[0] != black ){
             ropeKeypoints.push_back(keypoints[i]);
-            circle(original, pixel, 1, CV_RGB(0,0,255),2);
+            circle(original, pixel, 3, CV_RGB(0,0,255),2);
         }
+         else if(colour.val[0] != black){
+                keypoints3.push_back(keypoints[i]);
+                //circle(keypointsImg2, pixel, 1, CV_RGB(255,156,0),2);
+                //cout << keypoints[i].response << endl;
+            }   
+
+
          
     }
 
 
+    //calculate distance
+   std::sort(keypoints3.begin(), keypoints3.end(), response_comparator);
+   float distance [keypoints3.size()];
 
+   for (size_t i = 1; i < keypoints3.size(); ++i){
 
+        distance[i] = keypoints3[i].pt.x-keypoints3[i-1].pt.x;
+        //cout << distance[i] << endl;
+   }
+
+   int n = sizeof(distance)/sizeof(distance[0]);
+   sort(distance, distance+n); 
+   cout << "Distance:  " << -0.3/distance[int(n/2)] << endl;
+
+    std::stringstream ss;
+    ss << "Distance to net: " << -0.3/distance[int(n/2)] << "m";
+    string disp = ss.str();
+
+    putText(original, disp, Point(50, 100), FONT_HERSHEY_SIMPLEX, 1, CV_RGB(255,0,0), 4);
     drawKeypoints(original, keypoints3,original, CV_RGB(255,156,0));
     //drawKeypoints(rope, ropeKeypoints,rope, CV_RGB(255,0,0));
-    imshow( "winName2", original);
+    output_cap.write(original);
+    //imshow( "winName2", original);
     //imshow( "winName2", rope);
 
     
@@ -107,6 +140,7 @@ static void doIteration(Mat & img2, Ptr<FeatureDetector>& detector)
 int main(int argc, char** argv)
 {
 
+
     cv::initModule_nonfree();
 
     bool isWarpPerspective = argc == 7;
@@ -114,29 +148,32 @@ int main(int argc, char** argv)
     if( !isWarpPerspective )
         ransacReprojThreshold = atof(argv[7]);
 
-    cout << "< Creating detector, descriptor extractor and descriptor matcher ..." << endl;
     Ptr<FeatureDetector> detector = FeatureDetector::create( argv[1] );
 
-
-    cout << "< Reading the images..." << endl;
-    Mat img1 = imread( argv[5] ), img2;
+    Mat img2;
     if( !isWarpPerspective )
         img2 = imread( argv[6] );
 
 
-    doIteration(img2,detector);
-    for(;;)
-    {
-        char c = (char)waitKey(0);
-        if( c == '\x1b' ) // esc
-        {
-            cout << "Exiting ..." << endl;
-            break;
-        }
-        else if( isWarpPerspective )
-        {
-            doIteration( img2,detector);
-        }
+cout << "opening video" << endl;
+ VideoCapture inputVideo( "../night.mp4");
+    if(!inputVideo.isOpened()) { // check if we succeeded
+        cout << "fail open video" << endl;
+        return -1;
     }
-    return 0;
+
+cv::VideoWriter output_cap("../video.avi", CV_FOURCC('M','J','P','G'), 30, Size(1920, 1080));
+int i = 0;
+    for(;;){
+
+        Mat frame;
+        inputVideo >> frame; 
+        if(i == 800) break;
+        doIteration(frame, detector, output_cap );
+        if(waitKey(1) >= 0) break;
+        i++;
+    }
+
+    output_cap.release();
+return 0;
 }
